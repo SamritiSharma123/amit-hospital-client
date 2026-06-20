49bf08bf3d104a096e2b6489e0dbd0a2f48c0818
SamritiSharma123 <samritisharma270@gmail.com>
2026-06-20 13:08:26 +0530

    Fixed loader transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Loader from "./components/Loader";
 import Home from "./pages/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);
-    }, 4000);   // test with 4 sec first
+      setShowLoader(false);
+    }, 5000); // change to 60000 if you really want 60 sec
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
-      {loading ? <Loader /> : <Home />}
+      {showLoader ? <Loader /> : <Home />}
     </>
   );
 }
